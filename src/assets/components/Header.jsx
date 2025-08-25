79922086f341efdd67495682ac54cac9f990151f
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-25 14:35:17 +0200

    [add] Header.jsx, img folder + img files

---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+	<header>
+		<div className="container-fluid">
+			<div className="header-col">
+				<div id="logo">
+					<img src="" alt="" />
+				</div>
+			</div>
+		</div>
+	</header>
+  )
+}
+
+export default Header

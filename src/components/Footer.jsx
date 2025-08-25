da84b349a3c4c7ff739e3c7ed996780619f8c1b6
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-26 00:39:27 +0200

    [upd] styles, added elements to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,30 +3,78 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="container">
-    <div className="container-blue">
-      <div className="card">
-        <img src="public\img\buy-comics-digital-comics.png" alt="" />
-        <p>DIGITAL COMICS</p>
+      <div className="container-blue">
+        <div className="card">
+          <img src="public\img\buy-comics-digital-comics.png" alt="" />
+          <p>DIGITAL COMICS</p>
+        </div>
+        <div className="card">
+          <img src="public\img\buy-comics-merchandise.png" alt="" />
+          <p>DC MERCHANDISE</p>
+        </div>
+        <div className="card">
+          <img src="public\img\buy-comics-subscriptions.png" alt="" />
+          <p>SUBSCRIPTION</p>
+        </div>
+        <div className="card">
+          <img src="public\img\buy-comics-shop-locator.png" alt="" />
+          <p>COMIC SHOP LOCATOR</p>
+        </div>
+        <div className="card">
+          <img src="public\img\buy-dc-power-visa.svg" alt="" />
+          <p>DC POWER VISA</p>
+        </div>
       </div>
-      <div className="card">
-        <img src="public\img\buy-comics-merchandise.png" alt="" />
-        <p>DC MERCHANDISE</p>
-      </div>
-      <div className="card">
-        <img src="public\img\buy-comics-subscriptions.png" alt="" />
-        <p>SUBSCRIPTION</p>
-      </div>
-      <div className="card">
-        <img src="public\img\buy-comics-shop-locator.png" alt="" />
-        <p>COMIC SHOP LOCATOR</p>
-      </div>
-      <div className="card">
-        <img src="public\img\buy-dc-power-visa.svg" alt="" />
-        <p>DC POWER VISA</p>
-	  </div>
+      <div className="container-background">
+		<div className="container-fluid">
+		<ul>
+		<h2>
+			DC COMICS
+		</h2>
+			<li>Characters</li>
+			<li>Comics</li>
+			<li>Movies</li>
+			<li>TV</li>
+			<li>Games</li>
+			<li>Videos</li>
+			<li>News</li>
+		</ul>
+		<ul>
+		<h2>
+			SHOP
+		</h2>
+			<li>SHOP DC</li>
+			<li>SHOP DC Collectibles</li>
+		</ul>
+		<ul>
+		<h2>
+			DC
+		</h2>
+			<li>Terms Of Use</li>
+			<li>Privacy Policy(New)</li>
+			<li>Ad Choices</li>
+			<li>Advertising</li>
+			<li>Jobs</li>
+			<li>Subscriptions</li>
+			<li>CPSC Certificates</li>
+			<li>Ratings</li>
+			<li>Shop Help</li>
+			<li>Contact Us</li>
+		</ul>
+		<ul>
+		<h2>
+			SITES
+		</h2>
+			<li>DC</li>
+			<li>MAD Magazine</li>
+			<li>DC Kids</li>
+			<li>DC Universe</li>
+			<li>DC Power Visa</li>
+		</ul>
+		</div>
 	  </div>
       <div className="container-grey">
-		<p>lalala</p>
+        <p>lalala</p>
       </div>
     </footer>
   );

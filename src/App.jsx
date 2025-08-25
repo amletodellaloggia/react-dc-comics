4fdbaa505e9641a3be90156bff0539a62b58b3e7
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-25 17:55:03 +0200

    [upd] index.css (styles) [add] Footer.jsx, App.jsx (added import)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // Importazione componenti
 import Main from "./components/Main";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
   <>
   <Header />
   <Main />
+  <Footer />
   </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+	<footer className='container'>
+		<div className="card">
+		<img src="public\img\buy-comics-digital-comics.png" alt="" />
+		<p>DIGITAL COMICS</p>
+		</div>
+		<div className="card">
+		<img src="public\img\buy-comics-merchandise.png" alt="" />
+		<p>DC MERCHANDISE</p>
+		</div>
+		<div className="card">
+		<img src="public\img\buy-comics-subscriptions.png" alt="" />
+		<p>SUBSCRIPTION</p>
+		</div>
+		<div className="card">
+		<img src="public\img\buy-comics-shop-locator.png" alt="" />
+		<p>COMIC SHOP LOCATOR</p>
+		</div>
+		<div className="card">
+		<img src="public\img\buy-dc-power-visa.svg" alt="" />
+		<p>DC POWER VISA</p>
+		</div>
+	<div className="links-container">
+		
+	</div>
+	</footer>
+  )
+}
+
+export default Footer

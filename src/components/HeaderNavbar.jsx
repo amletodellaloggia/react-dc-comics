bdb9fb89e00b55bb264207a69c5942dae32313da
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-26 20:36:22 +0200

    [upd] added JSX map iteration

--- src/components/HeaderNavbar.jsx
+++ src/components/HeaderNavbar.jsx
@@ -1,39 +1,17 @@
 import React from "react";
 
 const HeaderNavbar = () => {
+	const navbarLinks = ["Characters", "Comics", "Movies", "TV", "Games", "Collectibles", "Videos", "Fans", "News", "Shop"]
   return (
     <div className="col-75">
       <ul id="menu">
-        <li>
-          <a href="#">Characters</a>
-        </li>
-        <li>
-          <a href="#">Comics</a>
-        </li>
-        <li>
-          <a href="#">Movies</a>
-        </li>
-        <li>
-          <a href="#">TV</a>
-        </li>
-        <li>
-          <a href="#">Games</a>
-        </li>
-        <li>
-          <a href="#">Collectibles</a>
-        </li>
-        <li>
-          <a href="#">Videos</a>
-        </li>
-        <li>
-          <a href="#">Fans</a>
-        </li>
-        <li>
-          <a href="#">News</a>
-        </li>
-        <li>
-          <a href="#">Shop</a>
-        </li>
+		{navbarLinks.map((link, index) => {
+			return (
+				<li key={index}>
+					<a href="#">{link}</a>
+				</li>
+			)
+		})}
       </ul>
     </div>
   );

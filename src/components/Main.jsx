07a4772d9f13df03bab199aa1a20bba1cad38870
amletodellaloggia <amletodellaloggia@gmail.com>
2025-08-26 13:18:49 +0200

    [upd] readme.md (exercise description), index.css (styles), comics.js (temporary), Main.jsx (added jumbotron, container, row, col)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 const Main = () => {
   return (
 	<main>
-
+		<div className="jumbotron">
+		<img src="public\img\jumbotron.jpg" alt="" />
+		</div>
+		<div className="container-fluid">
+			<div className="row">
+				
+			</div>
+		</div>
 	</main>
   )
 }
